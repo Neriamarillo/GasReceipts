57927d6fdb76c14b4323d764820a64d9c340ae56
Neriamarillo <jn769@nova.edu>
2018-12-01 06:19:45 -0500

    OCR Implemented and working. Original images are saved in OriginalPictures folder.

--- app/src/main/java/com/jn769/gasreceipts/MainActivity.java
+++ app/src/main/java/com/jn769/gasreceipts/MainActivity.java
@@ -1,30 +1,54 @@
 package com.jn769.gasreceipts;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "MainActivity";
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    String mCurrentPhotoPath;
+    private static final int REQUEST_WRITE_PERMISSION = 20;
+    private File storageDir;
+    private String mCurrentPhotoPath;
+    private File image;
+    private TextView processedText;
+    private Uri photoURI;
+    private TextRecognizer recognizer;
+    private SparseArray<TextBlock> textBlocks = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +57,49 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        storageDir = new File(Environment.getExternalStorageDirectory(),
+                "Android/data/com.jn769.gasreceipts/files/OriginalReceipts");
+        Button button = (Button) findViewById(R.id.button);
+
+        processedText = (TextView) findViewById(R.id.results);
+        recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dispatchTakePictureIntent();
                 Snackbar.make(view, "LOADING CAMERA!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                ActivityCompat.requestPermissions(MainActivity.this, new
+                        String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
             }
         });
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "LOADING CAMERA!", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    // Request android permission, then take picture if passed
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(LOG_TAG, storageDir.getAbsolutePath());
+                    Log.i(LOG_TAG, String.valueOf(storageDir.exists()));
+                    if (!storageDir.exists()) {
+                        storageDir.mkdirs();
+                    }
+                    dispatchTakePictureIntent();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 
     @Override
@@ -75,14 +133,13 @@ public class MainActivity extends AppCompatActivity {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
-                galleryAddPic();
             } catch (IOException ex) {
                 // Error occurred while creating the File
                 ex.printStackTrace();
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(
+                photoURI = FileProvider.getUriForFile(
                         this,
                         "com.jn769.gasreceipts.fileprovider",
                         photoFile);
@@ -92,34 +149,98 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    // Create image file name
     private File createImageFile() throws IOException {
-        // Create image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = new File(Environment.getExternalStorageDirectory(),
-                "Android/data/com.jn769.gasreceipts/files/OriginalReceipts");
-        storageDir.mkdirs();
-        File image = File.createTempFile(
+        Log.i(LOG_TAG, "File name:" + imageFileName);
+        Log.i(LOG_TAG, String.valueOf(storageDir.exists()));
+        image = File.createTempFile(
                 imageFileName,      /* prefix */
                 ".jpg",      /* suffix */
                 storageDir          /* directory */
         );
-
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
     }
 
+    // OCR decoding process
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            galleryAddPic();
+            try {
+                Bitmap bitmap = decodeBitmapUri(this, photoURI);
+                Log.i(LOG_TAG, String.valueOf(recognizer.isOperational()));
+                if (recognizer.isOperational() && bitmap != null) {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    textBlocks = recognizer.detect(frame);
+                    String blocks = "";
+                    String lines = "";
+                    String words = "";
+                    for (int index = 0; index < textBlocks.size(); index++) {
+                        //extract scanned text blocks here
+                        TextBlock tBlock = textBlocks.valueAt(index);
+                        blocks = blocks + tBlock.getValue() + "\n" + "\n";
+                        for (Text line : tBlock.getComponents()) {
+                            //extract scanned text lines here
+                            lines = lines + line.getValue() + "\n";
+                            for (Text element : line.getComponents()) {
+                                //extract scanned text words here
+                                words = words + element.getValue() + ", ";
+                            }
+                        }
+                    }
+                    if (textBlocks.size() == 0) {
+                        processedText.setText("Scan Failed: Found nothing to scan");
+                    } else {
+                        processedText.setText(processedText.getText() + "Blocks: " + "\n");
+                        processedText.setText(processedText.getText() + blocks + "\n");
+                        processedText.setText(processedText.getText() + "---------" + "\n");
+                        processedText.setText(processedText.getText() + "Lines: " + "\n");
+                        processedText.setText(processedText.getText() + lines + "\n");
+                        processedText.setText(processedText.getText() + "---------" + "\n");
+//                        processedText.setText(processedText.getText() + "Words: " + "\n");
+//                        processedText.setText(processedText.getText() + words + "\n");
+//                        processedText.setText(processedText.getText() + "---------" + "\n");
+                    }
+                } else {
+                    processedText.setText("Recognizer not set up!");
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e(LOG_TAG, e.toString());
+            }
+        }
+    }
+
     // Make image available to external gallery
     private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(mCurrentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
+        File file = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(file);
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, contentUri);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
     }
 
+    // Decode bitmap for ocr use
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
 
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
 
 }
